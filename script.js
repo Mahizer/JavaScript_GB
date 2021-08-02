9c1c6c94532f00e493ceba10cfefbebd3c38075c
Davudova <mahizer-mamedova@mail.ru>
2021-08-02 15:28:27 +0300

    lesson4

--- script.js
+++ script.js
@@ -1,120 +1,67 @@
 'use strict';
+/* 1.1 */
 
-/* 1 С помощью цикла for написать алгоритм для вывода чисел (выводите в консоль, с помощью console.log) от 0 до 10
-включительно */
+function Product(name, price) {
+    this.name = name;
+    this.price = price;
+}
+Product.prototype.make25PercentDiscount = function () {
+    this.price = this.price - (this.price * 0.75);
+};
 
-for (let i = 0; i <= 10; i++) {
-    if (i == 0) {
-        console.log(`${i} - это ноль`);
-    } else if (i % 2 == 0) {
-        console.log(`${i} - это четное число`);
-    } else {
-        console.log(`${i} - это нечетное число`);
+const my_product = new Product('Шкаф', 4320);
+my_product.make25PercentDiscount();
+console.log(my_product);
+
+/* 1.1 */
+
+class Product2 {
+    constructor(name, price) {
+        this.name = name;
+        this.price = price;
+    }
+    make25PercentDiscount() {
+        this.price = this.price - (this.price * 0.75);
     }
 }
 
+const my_product2 = new Product2('Шкаф', 4320);
+my_product2.make25PercentDiscount();
+console.log(my_product2);
 
-/* 2 */
+/* */
 
-const post = {
-    author : 'John',
-    postId : 23,
-    comments : [
-        {
-            userId: 10,
-            userName: 'Alex',
-            text: 'Lorem ipsum',
-            rating: {
-                likes: 10,
-                dislikes: 2
-            }
-        },
-        {
-            userId: 5,
-            userName: 'Jane',
-            text: 'Lorem ipsum 2',
-            rating: {
-                likes: 3,
-                dislikes: 1
-            }
-        },
-
-    ]
-};
-
-console.log(post.author);
-console.log(post.comments[0].rating.dislikes);
-console.log(post.comments[1].userId);
-console.log(post.comments[1].text);
-
-
-/* 3 */
-
-const products = [
-    {
-        id: 3,
-        price: 200,
-    },
-    {
-        id: 4,
-        price: 900,
-    },
-    {
-        id: 1,
-        price: 1000,
-    },
-];
-
-for  (let i = 0; i < products.length; i++ ) {
-    console.log(products[i].price * 0.85);
-};
-
-/* 4 */
-
-const product = [
-    {
-        id: 3,
-        price: 127,
-        photos: [
-            '1.jpg',
-            '2.jpg'
-        ]
-    },
-    {
-        id: 5,
-        price: 499,
-        photos: []
-    },
-    {
-        id: 10,
-        price: 26,
-        photos: [
-            '3.jpg'
-        ]
-    },
-    {
-        id: 8,
-        price: 78
+class Post {
+    constructor(author, text, date) {
+        this.author = author;
+        this.text = text;
+        this.date = date;
     }
-];
 
-for (let i = 0; i < product.length; i ++) {
-    if (product[i].photos > Array(0)) {
-    console.log(product[i])
-    };
-};
-
-let my_arr = []
-
-for (let i = 0; i <product.length; i ++) {
-    my_arr.push(product[i].price)
+    edit(text) {
+        this.text = text;
+    }
 }
 
-console.log(my_arr.sort((a,b) => a - b))
+const my_post = new Post('zara', 'zara1', new Date());
+console.log(my_post);
+my_post.edit('text');
+console.log(my_post);
 
 
-/* 6 */
+class AttachedPost extends Post {
+    constructor(author, text, date) {
+        super(author, text, date);
+        this.highlighted = false;
+    }
 
-for (let i = 1; i <= 5; i++) {
-    console.log('*'.repeat(i));
-};
+    makeTextHighlighted() {
+        this.highlighted = true;
+    }
+}
+
+const attached1 = new AttachedPost('zara2', 'zara2', new Date());
+console.log(attached1);
+attached1.makeTextHighlighted();
+attached1.edit('texttext');
+console.log(attached1);
